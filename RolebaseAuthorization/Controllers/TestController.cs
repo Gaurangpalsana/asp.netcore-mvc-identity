8107dd6deb50190cd749b6b73532d79b21ff2514
Gaurang palsana <Gaurangpalsana007@gmail.com>
2018-12-11 16:27:13 +0530

    commit of authorization

---
+++ RolebaseAuthorization/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace RolebaseAuthorization.Controllers
+{
+    public class TestController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult OnlyAdminAccess()
+        {
+            ViewData["role"] = "Admin";
+            return View("OnlyAdminAccess");
+        }
+
+
+        [Authorize(Roles = "User,HR")]
+        public IActionResult OnlyUserAccess()
+        {
+            ViewData["role"] = "User";
+            return View("MyPage");
+        }
+        [Authorize(Roles = "HR")]
+        public IActionResult OnlyHRAccess()
+        {
+            ViewData["role"] = "HR";
+            return View("OnlyHRAccess");
+        }
+
+        [Authorize(Policy = "OnlyAdminAccess")]
+        public IActionResult PolicyExample()
+        {
+            ViewData["role"] = "Admin";
+            return View("MyPage");
+        }
+    }
+}
